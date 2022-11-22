bfc5693af2ae2f5ac131f4bae8cfa5b8226d0f63
nalee97 <mohamednaleef1564@gmail.com>
2022-11-22 10:54:56 +0530

    commit

--- GrphQL/Mutation.cs
+++ GrphQL/Mutation.cs
@@ -22,4 +22,28 @@ public class Mutation
         return movie;
     }
 
+    [GraphQLName("UpdateMovie")]
+    [UseDbContext(typeof(SuperHeroDbContext))]
+    public async Task<Movie> UpdateMovieAsync(Movie input, [ScopedService] SuperHeroDbContext context,
+        CancellationToken token)
+    {
+        var movie = await context.Movies.FindAsync(input.Id);
+
+        if (movie is not null)
+        {
+            movie.Title = input.Title;
+            movie.Description = input.Description;
+            movie.ReleaseDate = input.ReleaseDate;
+            movie.Instructor = input.Instructor;
+        }
+        else
+        {
+            return null;
+        }
+
+        context.Movies.Update(movie);
+        await context.SaveChangesAsync(token);
+
+        return movie;
+    }
 }
