d9a9001d03c092c52c92594ae35521c2c9bf5f7f
nalee97 <mohamednaleef1564@gmail.com>
2022-11-26 11:52:31 +0530

    commit

---
+++ GrphQL/Controllers/MovieController.cs
@@ -0,0 +1,53 @@
+﻿using GrphQL.IRepository;
+using GrphQL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrphQL.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieRepository _movieRepo;
+
+        public MovieController(IMovieRepository movieRepo)
+        {
+            _movieRepo = movieRepo;
+        }
+
+        [HttpGet]
+        [Route("api/movies")]
+        public async Task<IActionResult> GetMovies()
+        {
+            var movies = await _movieRepo.GetMovies();
+
+            return Ok(movies);
+        }
+
+        [HttpGet]
+        [Route("api/movies/{id}")]
+        public async Task<IActionResult> GetMovie(Guid id)
+        {
+            var movie = await _movieRepo.GetMovie(id);
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        [Route("api/movies")]
+        public async Task<IActionResult> AddMovie([FromBody] Movie movie)
+        {
+            await _movieRepo.AddMovie(movie);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/movies")]
+        public async Task<IActionResult> UpdateMovie([FromBody] Movie movie)
+        {
+            var Movie = await _movieRepo.UpdateMovie(movie);
+
+
+            await _movieRepo.UpdateMovie(movie);
+            return Ok();
+        }
+
+    }
+}
---
+++ GrphQL/Controllers/SuperpowerController.cs
@@ -0,0 +1,52 @@
+﻿using GrphQL.IRepository;
+using GrphQL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrphQL.Controllers
+{
+    public class SuperpowerController : Controller
+    {
+        private readonly ISuperpowerRepository _superpowerRepo;
+
+        public SuperpowerController(ISuperpowerRepository superpowerRepo)
+        {
+            _superpowerRepo = superpowerRepo;
+        }
+
+        [HttpGet]
+        [Route("api/superpowers")]
+        public async Task<IActionResult> GetSuperpowers()
+        {
+            var superpowers = await _superpowerRepo.GetSuperpowers();
+
+            return Ok(superpowers);
+        }
+
+        [HttpGet]
+        [Route("api/superpowers/{id}")]
+        public async Task<IActionResult> GetSuperpower(Guid id)
+        {
+            var superpower = await _superpowerRepo.GetSuperpower(id);
+            return Ok(superpower);
+        }
+
+        [HttpPost]
+        [Route("api/superpowers")]
+        public async Task<IActionResult> AddSuperpower([FromBody] Superpower superpower)
+        {
+            await _superpowerRepo.AddSuperpower(superpower);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("api/superpowers")]
+        public async Task<IActionResult> UpdateSuperpower([FromBody] Superpower superpower)
+        {
+            var superPower = await _superpowerRepo.UpdateSuperpower(superpower);
+
+
+            await _superpowerRepo.UpdateSuperpower(superPower);
+            return Ok();
+        }
+    }
+}
