4c0d6d33c18993fab792290e25cb3e86bcecee49
nalee97 <mohamednaleef1564@gmail.com>
2022-11-26 12:01:18 +0530

    commit

---
+++ GrphQL/Models/UpdateSuperheroInput.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrphQL.Models
+{
+    public class UpdateSuperheroInput
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Height { get; set; }
+    }
+}
---
+++ GrphQL/Models/UpdateSuperpowerInput.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GrphQL.Models
+{
+    public class UpdateSuperpowerInput
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string SuperPower { get; set; }
+        public string? Description { get; set; }
+    }
+}
